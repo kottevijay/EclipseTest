8ffbfd6a1acce09dd62d07ce5770ab5da59aac14
kottevijay <kottevijay@Gmail.com>
2024-12-21 17:09:31 +0530

    Project1 Added

---
+++ Project1/src/pack2/Class4.java
@@ -0,0 +1,11 @@
+package pack2;
+
+public class Class4 {
+
+	public static void main(String[] args) {
+		System.out.println("first line code...");
+		System.out.println("second line code...");
+		System.out.println("Third line code...");
+	}
+
+}
